d9ae7ad327696d34d3e0132effb0084e9fb18f64
Denice M <denicem@hotmail.de>
2023-01-30 17:38:21 +0100

    add data structure and funcs to test vector.

---
+++ inc/Vect3.hpp
@@ -0,0 +1,15 @@
+#ifndef VECT3_HPP
+#define VECT3_HPP
+
+struct Vect3 {
+	int x, y, z;
+
+	Vect3(int x, int y, int z): x(x), y(y), z(z) {}
+
+	friend std::ostream& operator<<(std::ostream& stream, vect3& v3) {
+		stream << "[" << v3.x << " " << v3.y << " " << v3.z << "]";
+		return (stream);
+	}
+};
+
+#endif
