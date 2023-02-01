5a22debab9e7c5db24307d48d49e10fd3525c39b
denicem <denicem@hotmail.de>
2023-02-01 03:02:56 +0100

    add TODO test cases.

--- inc/test_vector.hpp
+++ inc/test_vector.hpp
@@ -24,3 +24,40 @@ void vector_test_basic_ft() {
 }
 
 #endif
+
+/*
+Default constructor: TODO:
+	Test that the default constructor initializes the vector
+	with the correct size and capacity, and that it behaves
+	as expected when elements are added and removed.
+
+Size and capacity: TODO:
+	Test that the size and capacity member functions return
+	the correct values, and that the capacity increases
+	dynamically as elements are added to the vector.
+
+Element access: TODO:
+	Test that elements can be accessed using the operator[]
+	and the at member functions, and that out-of-range
+	accesses throw exceptions.
+
+Insertion and deletion: TODO:
+	Test that elements can be inserted and deleted at the end
+	of the vector, as well as at specific positions, and that
+	the size and capacity of the vector are updated as expected.
+
+Copy constructor and assignment operator: TODO:
+	Test that the copy constructor and assignment operator behave
+	as expected, creating deep copies of the vector and preserving
+	the elements and their values.
+
+Iterators: TODO:
+	Test that the iterators provided by the vector class work as
+	expected, and that they can be used to traverse the elements
+	of the vector.
+
+Allocator: TODO:
+	Test that the std::allocator class is used correctly,
+	allocating and deallocating memory as expected, and
+	constructing and destroying elements correctly.
+*/
