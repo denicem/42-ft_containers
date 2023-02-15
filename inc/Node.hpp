13c86a8f64d0f9d25803e5532bdeb9bfde0b0cfb
denicem <denicem@hotmail.de>
2023-02-15 15:41:34 +0100

    remove unnecessary stuff.

--- inc/Node.hpp
+++ inc/Node.hpp
@@ -5,19 +5,16 @@ namespace ft {
 
 template < typename T >
 struct Node {
-	typedef T							value_type;
-	typedef value_type*		pointer;
+	typedef T					value_type;
+	typedef value_type*			pointer;
 	typedef Node<value_type>*	node_pointer;
 
 	value_type data;
 	node_pointer parent;
 	node_pointer left;
 	node_pointer right;
-	int bf;
-	int height;
 
-	// Node(): data(value_type()), parent(NULL), left(NULL), right(NULL), bf(0) {}
-	Node(const value_type& data = value_type()): data(data), parent(NULL), left(NULL), right(NULL), bf(0), height(0) {}
+	Node(const value_type& data = value_type()): data(data), parent(NULL), left(NULL), right(NULL) {}
 
 };
 
