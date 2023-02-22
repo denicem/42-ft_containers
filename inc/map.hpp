7fe60d5d08958423a0be65353098d0b1ab3b2a09
denicem <denicem@hotmail.de>
2023-02-22 15:28:06 +0100

    structure code for map.

--- inc/map.hpp
+++ inc/map.hpp
@@ -16,13 +16,11 @@ template <	class Key,
 		 >
 class map {
 
-	/*
-	** ----------------------- MEMBER TYPES -----------------------
-	*/
+	/* -------------------------------------------------------------------------- */
+	/*                                MEMBER TYPES                                */
+	/* -------------------------------------------------------------------------- */
+
 	public:
-		// typedef Alloc													allocator_type;
-		// typedef typename allocator_type::value_type::first_type			key_type; // NOTE: is that possible?
-		// typedef typename allocator_type::value_type::second_type		mapped_type; // NOTE: is that possible?
 		typedef Key											key_type;
 		typedef T											mapped_type;
 		typedef ft::pair<const key_type, mapped_type>		value_type;
@@ -33,18 +31,14 @@ class map {
 		typedef typename allocator_type::const_reference	const_reference;
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
-		// typedef ft::MapIterator<value_type> 					iterator;
-		// typedef ft::MapIterator<const value_type> 			const_iterator;
-		// typedef ft::MapIterator<iterator> 					reverse_iterator;
-		// typedef ft::MapIterator<const_iterator> 				const_reverse_iterator;
-		// typedef typename iterator_traits<iterator>::difference_type	difference_type;
-		// typedef typename iterator_traits<iterator>::size_type		size_type;
+		
 		typedef typename allocator_type::size_type size_type;
 
 	
-		/*
-		** ----------------------- VALUE COMPARE -----------------------
-		*/
+		/* -------------------------------------------------------------------------- */
+		/*                                VALUE COMPARE                               */
+		/* -------------------------------------------------------------------------- */
+
 		class value_compare { // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
 			friend class ft::AVLTree<value_type, value_compare, allocator_type>;
 
@@ -67,17 +61,25 @@ class map {
 		typedef typename tree_type::iterator		iterator;
 		// typedef typename tree_type::const_iterator	const_iterator;
 
-	/*
-	** --------------------------- MEMBER VARIABLES ---------------------------
-	*/
+	/* -------------------------------------------------------------------------- */
+	/*                              MEMBER VARIABLES                              */
+	/* -------------------------------------------------------------------------- */
+
 	private:
 		tree_type	_tree;
 
-	/*
-	** ---------------------------- MEMBER FUNCTIONS ---------------------------
-	** ----------------------- CONSTRUCTORS & DESTRUCTOR -----------------------
-	*/
+	/* -------------------------------------------------------------------------- */
+	/*                              MEMBER FUNCTIONS                              */
+	/* -------------------------------------------------------------------------- */
+
+		/* -------------------------------------------------------------------------- */
+		/*                          CONSTRUCTORS & DESTRUCTOR                         */
+		/* -------------------------------------------------------------------------- */
+
+		/* ------------------------------ CONSTRUCTORS ------------------------------ */
+
 	public:
+
 		// empty/default constructor
 		explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()): _tree(tree_type()) { (void) comp; (void) alloc; }
 		// range constructor
@@ -85,15 +87,41 @@ class map {
 		// copy constructor
 		// map (const map& x) {}
 
-		// map& operator=(const map& x) {}
-
+		/* ------------------------------- DESTRUCTOR ------------------------------- */
 		// ~map() {}
 
+		/* --------------------------- ASSIGNMENT OPERATOR -------------------------- */
+		// map& operator=(const map& x) {}
+
+
+		/* -------------------------------------------------------------------------- */
+		/*                                  ITERATORS                                 */
+		/* -------------------------------------------------------------------------- */
+
+	public:
+		iterator begin() { return (this->_tree.begin()); }
+		// const_iterator begin() const {}
+		iterator end() { return (this->_tree.end()); }
+		// const_iterator end() const {}
+
+		// reverse_iterator rbegin() {}
+		// const_reverse_iterator rbegin() const {}
+		// reverse_iterator rend() {}
+		// const_reverse_iterator rend() const {}
+
+		/* -------------------------------------------------------------------------- */
+		/*                                  CAPACITY                                  */
+		/* -------------------------------------------------------------------------- */
+
+		bool empty() const { return (this->_tree.empty()); }
 		size_type size() const { return (this->_tree.size()); }
+		// size_type max_size() const {}
 
 		/* -------------------------------------------------------------------------- */
 		/*                               ELEMENT ACCESS                               */
 		/* -------------------------------------------------------------------------- */
+
+	public:
 		mapped_type& operator[](const key_type& k) {
 			ft::pair<iterator, bool> res = this->insert(value_type(k, mapped_type()));
 			return ( (res.first)->second );
@@ -117,25 +145,72 @@ class map {
 		/*                                  MODIFIERS                                 */
 		/* -------------------------------------------------------------------------- */
 
+	public:
+
 		/* --------------------------------- INSERTS -------------------------------- */
-		/* ----------------------------- SINGLE ELEMENT ----------------------------- */
+		// single element
 		ft::pair<iterator, bool> insert(const value_type& val) {
 			return (this->_tree.insert(val));
 		}
-
-		/* -------------------------------- WITH HINT ------------------------------- */
+		// with hint
 		iterator insert(iterator pos, const value_type& val) {
 			(void) pos;
 			ft::pair<iterator, bool> res = this->insert(val);
 			return (res.first);
 		}
-
-		/* ---------------------------------- RANGE --------------------------------- */
+		// range
 		template < class InputIterator > // TODO: test if it works
 		void insert(InputIterator first, InputIterator last) {
 			for (; first != last; ++first)
 				this->insert(*first);
 		}
+		/* -------------------------------------------------------------------------- */
+
+		/* ---------------------------------- ERASE --------------------------------- */
+		// void erase(iterator position) {}
+		// size_type erase(const key_type& k) {}
+		// void erase(iterator first, iterator last) {}
+		/* -------------------------------------------------------------------------- */
+
+		// void swap(map& x) {}
+		// void clear();
+
+		/* -------------------------------------------------------------------------- */
+		/*                                  OBSERVERS                                 */
+		/* -------------------------------------------------------------------------- */
+
+		// key_compare key_comp() const {}
+		// value_compare value_comp() const {}
+
+		/* -------------------------------------------------------------------------- */
+		/*                                   LOOKUP                                   */
+		/* -------------------------------------------------------------------------- */
+
+		iterator find(const key_type& k) { return (this->_tree.find(k)); }
+		// const_iterator find(const key_type& k) const {}
+
+		size_type count(const key_type& k) const { return (this->_tree.find(k) != NULL ? 1 : 0); }
+
+		// iterator lower_bound(const key_type& k) {}
+		// const_iterator lower_bound(const key_type& k) const {}
+		// iterator upper_bound(const key_type& k) {}
+		// const_iterator upper_bound(const key_type& k) const {}
+
+		// ft::pair<const_iterator, const_iterator> equal_range (const key_type& k) const {}
+		// ft::pair<iterator, iterator> equal_range (const key_type& k) const {}
+
+		/* -------------------------------------------------------------------------- */
+		/*                                  ALLOCATOR                                 */
+		/* -------------------------------------------------------------------------- */
+
+		// allocator_type get_allocator() const {}
+
+		/* -------------------------- END MEMBER FUNCTIONS -------------------------- */
+
+		/* -------------------------------------------------------------------------- */
+
+		/* -------------------------------------------------------------------------- */
+		/*                                    OTHER                                   */
 		/* -------------------------------------------------------------------------- */
 
 		void comparePairs(value_type& p1, value_type& p2) {
@@ -147,10 +222,7 @@ class map {
 
 		void printMap() const { this->_tree.printTree(); }
 
-	public:
-		iterator begin() { return (this->_tree.begin()); }
-		iterator end() { return (this->_tree.end()); }
-};
+}; // END CLASS MAP
 
 } // END NAMESPACE FT
 
